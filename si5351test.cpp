36a1c6bb75c566a7daeee40abf1f201f7f20dc5b
Sean Victor Hum <sean.hum@utoronto.ca>
2024-08-20 22:20:10 -0400

    Update si5351 test for quadrature output

--- si5351test.cpp
+++ si5351test.cpp
@@ -8,7 +8,12 @@ Si5351 si5351(SI5351_I2C_ADDRESS);
 int main(void) {
 	unsigned long long ullFreqHz = 0;
 	si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0, 0);
-	si5351.set_freq(1200000000ULL, SI5351_CLK0);		// format: (desired_freq_hz*100, OUTPUT_PIN)
+	//si5351.set_freq(1200000000ULL, SI5351_CLK0);		// format: (desired_freq_hz*100, OUTPUT_PIN)
+	si5351.set_freq_manual(1407400000ULL, 70370000000ULL, SI5351_CLK0);		// format: (desired_freq_hz*100, OUTPUT_PIN)
+	//si5351.set_freq(1200000000ULL, SI5351_CLK1);		// format: (desired_freq_hz*100, OUTPUT_PIN)
+	si5351.set_freq_manual(1407400000ULL, 70370000000ULL, SI5351_CLK1);		// format: (desired_freq_hz*100, OUTPUT_PIN)
+	si5351.set_phase(SI5351_CLK0, 0);
+	si5351.set_phase(SI5351_CLK1, 50);
 
 	return(0);
 }
