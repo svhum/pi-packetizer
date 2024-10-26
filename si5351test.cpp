6b3b4eb58b7e7c9bdc12821bd00d6eef079b17a4
Sean Victor Hum <sean.hum@utoronto.ca>
2024-10-26 02:46:27 +0000

    Update si5351test for PA testing

--- si5351test.cpp
+++ si5351test.cpp
@@ -10,6 +10,8 @@
 Si5351 si5351(SI5351_I2C_ADDRESS);
 
 int main(void) {
+  uint64_t freq = 1407400000;
+
   // Initialize GPIO
   // Uses BCM numbering of the GPIOs and directly accesses the GPIO registers
   wiringPiSetupGpio();
@@ -19,14 +21,15 @@ int main(void) {
 
   // Set to TX
   digitalWrite (TXENPIN, HIGH);
-  digitalWrite (KEYPIN, HIGH);
+  digitalWrite (KEYPIN, LOW);
 
 	unsigned long long ullFreqHz = 0;
 	si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0, 0);
   //si5351.set_pll_input(SI5351_PLLA, SI5351_PLL_INPUT_CLKIN);
   //si5351.set_pll_input(SI5351_PLLB, SI5351_PLL_INPUT_CLKIN);
   // Test of  single-ended output
-	si5351.set_freq(1407400000ULL, SI5351_CLK2);		// format: (desired_freq_hz*100, OUTPUT_PIN)
+	//si5351.set_freq(freq, SI5351_CLK0);		// format: (desired_freq_hz*100, OUTPUT_PIN)
+	si5351.set_freq(freq, SI5351_CLK2);		// format: (desired_freq_hz*100, OUTPUT_PIN)
 	//si5351.set_freq(1400000000ULL, SI5351_CLK0);		// format: (desired_freq_hz*100, OUTPUT_PIN)
 	//si5351.set_freq(1400000000ULL, SI5351_CLK1);		// format: (desired_freq_hz*100, OUTPUT_PIN)
 	//si5351.set_freq(1407400000ULL, SI5351_CLK2);		// format: (desired_freq_hz*100, OUTPUT_PIN)
@@ -48,6 +51,18 @@ int main(void) {
 	//si5351.set_freq_manual(1407400000ULL, 70370000000ULL, SI5351_CLK1);		// format: (desired_freq_hz*100, OUTPUT_PIN)
 	//si5351.set_phase(SI5351_CLK0, 0);
 	//si5351.set_phase(SI5351_CLK1, 50);
+
+  sleep(1);
+  //digitalWrite (KEYPIN, HIGH);
+  for (int k=0; k < 100; k++) {
+    freq = freq + 100;
+    printf("freq = %d\n", freq);
+	  //si5351.set_freq(freq, SI5351_CLK0);		// format: (desired_freq_hz*100, OUTPUT_PIN)
+	  si5351.set_freq(freq, SI5351_CLK2);		// format: (desired_freq_hz*100, OUTPUT_PIN)
+    usleep(100000);
+  }
+  digitalWrite (KEYPIN, LOW);
+  digitalWrite (TXENPIN, LOW);
 
 	si5351.update_status();
 	usleep(500000);
